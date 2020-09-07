20549ccd91389a1d05d61cb6699ea98655ab4f48
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-09-07 20:14:06 +0200

    Remove empty oninit on listeelmement

--- src/app/listelement/listelement.component.ts
+++ src/app/listelement/listelement.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lis',
   templateUrl: './listelement.component.html',
   styleUrls: ['./listelement.component.less']
 })
-export class ListelementComponent implements OnInit {
+export class ListelementComponent {
   @Input() desc;
   @Input() uri;
   constructor() { }
-  ngOnInit(): void {
-  }
 }
